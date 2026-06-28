88cc1dd0e5ca2d5f37f26eb2f405f403011948bb
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-28 14:30:24 +0530

    Code Update

--- app/[locale]/service/page.js
+++ app/[locale]/service/page.js
@@ -1,13 +1,10 @@
-import { Footer, Header, BannerHome, HeaderSearch, ProductList, ProductCategory, BannerCategory, ProductRegular, TotalSale, OurService, FooterMenu, Testimonial, BannerStatic } from "@/components/common";
+// import { Footer, Header, BannerHome, HeaderSearch, ProductList, ProductCategory, BannerCategory, ProductRegular, TotalSale, OurService, FooterMenu, Testimonial, BannerStatic } from "@/components/common";
 
 
 export default function ProductPage() {
   return (
     <>
-      <BannerStatic pagename="Services" />
-      <OurService />
-      <TotalSale />
-      <Testimonial />
+      <h1>Services</h1>
     </>
   );
 }
