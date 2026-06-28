436163124d87c8c227a249767aa95bc41328a8e3
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-28 15:56:04 +0530

    Code Updated

--- app/components/LanguageSwitcher.tsx
+++ app/components/LanguageSwitcher.tsx
@@ -1,22 +1,22 @@
 'use client';
 
-// import Link from 'next/link';
 import { Link } from '@/i18n/navigation';
 import { usePathname } from 'next/navigation';
 import { useLocale } from 'next-intl';
 
 const languages = [
-  { code: "ar", name: "العربية", flag: "🇸🇦" },
+  { code: 'ar', name: 'العربية', flag: '🇸🇦' },
   { code: 'en', name: 'English', flag: '🇬🇧' },
   { code: 'hi', name: 'हिन्दी', flag: '🇮🇳' },
-  { code: 'bn', name: 'বাংলা', flag: '🇧🇩' },
+  { code: 'bn', name: 'বাংলা', flag: '🇧🇩' }
 ];
 
 export default function LanguageSwitcher() {
   const pathname = usePathname();
   const locale = useLocale();
 
-  const currentLanguage = languages.find(lang => lang.code === locale) || languages[0];
+  const currentLanguage =
+    languages.find(lang => lang.code === locale) || languages[0];
 
   return (
     <>
@@ -26,24 +26,22 @@ export default function LanguageSwitcher() {
         data-bs-toggle="dropdown"
       >
         <i className="fas fa-globe fa-2x"></i>
-        <span className="ms-2">
-          {currentLanguage.flag}
-        </span>
-      </a>    
-    
-    <ul className="dropdown-menu">
-      {languages.map((lang) => (
-        <li key={lang.code}>
-          <Link
-            className="dropdown-item text-dark"
-            href={`/${pathname.replace(/^\/(en|hi|bn)/, '')}`}
-            locale={lang.code}
-          >
-            {lang.flag} {lang.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
+        <span className="ms-2">{currentLanguage.flag}</span>
+      </a>
+
+      <ul className="dropdown-menu">
+        {languages.map((lang) => (
+          <li key={lang.code}>
+            <Link
+              href={`/${pathname.replace(/^\/(en|hi|bn|ar)/, '')}`}
+              locale={lang.code}
+              className="dropdown-item text-dark"
+            >
+              {lang.flag} {lang.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
