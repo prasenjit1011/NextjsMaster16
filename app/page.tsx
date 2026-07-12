eb33312b7ef7e393867515d847dfd7c1be1df7bb
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-12 14:08:35 +0530

    123

--- app/page.tsx
+++ app/page.tsx
@@ -8,28 +8,7 @@ export default function Home() {
         src="/property.png"
         alt="Property"
         className="block w-full h-auto"
-      />
-      <Image
-        src="https://drive.google.com/uc?export=view&id=1D67boLOfITOsQfbU0r6qbfdPCCO_7zMh"
-        alt="Property"
-        width={1920}
-        height={1080}
-        priority
-        unoptimized
-        className="block w-full h-auto"
-      />
-
-      <hr className="my-8 border-gray-300" />
-
-      {/* Local Image */}
-      <Image
-        src="/property.png"
-        alt="Property"
-        width={1920}
-        height={1080}
-        priority
-        className="block w-full h-auto"
-      />
+      />      
     </main>
   );
 }
