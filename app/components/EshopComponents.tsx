a1369b8155cbf78dcd216cb94c64f109549288e1
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-07-13 11:26:02 +0530

    Commit

--- app/components/EshopComponents.tsx
+++ app/components/EshopComponents.tsx
@@ -17,8 +17,9 @@ interface BannerProps {
 }
 
 const Banner = ({ pageTitle }: BannerProps) => {
+    
     if(pageTitle === 'Home'){
-        return <BannerHome />
+        // return <BannerHome />
     }
 
     return (
@@ -510,7 +511,7 @@ export const ProductList = () =>{
 
     let products = [];
     for (let i = 0; i < 9; i++) {
-        products.push(<Product boxClassName="col-md-6 col-lg-4 col-xl-3" />);
+        products.push(<Product key={i} boxClassName="col-md-6 col-lg-4 col-xl-3" />);
     }
 
 
@@ -619,7 +620,7 @@ function Product({ boxClassName }: ProductProps) {
 
     
     return (
-        <div className={boxClassName}>
+        <div className={boxClassName} key={'product'+1000*Math.random()}>
             <div className="rounded position-relative fruite-item">
                 <div className="fruite-img">
                     <img src={imgName} className="img-fluid w-100 rounded-top" alt="" />
@@ -640,11 +641,56 @@ function Product({ boxClassName }: ProductProps) {
 
 
 export const ProductListing = () => {
-    return <h1>PLIST</h1>
+
+            <>
+            <div className="container-fluid zpage-header py-5"></div>
+            <div className="container-fluid fruite py-5">
+                <div className="container py-5">
+                    <div className="row g-4">
+                        <div className="col-lg-12">
+                            <div className="row g-4">
+
+                                <div className="col-xl-3">
+                                    {/* <ProductSearch boxWidth="input-group w-100 mx-auto d-flex" /> */}
+                                </div>
+                                <div className="col-6"></div>
+                                <div className="col-xl-3">
+                                    <div className="bg-light ps-3 py-3 rounded d-flex justify-content-between mb-4">
+                                        <label for="fruits">Default Sorting:</label>
+                                        <select id="fruits" name="fruitlist" className="border-0 form-select-sm bg-light me-3" form="fruitform">
+                                            <option value="volvo">Nothing</option>
+                                            <option value="saab">Popularity</option>
+                                            <option value="opel">Organic</option>
+                                            <option value="audi">Fantastic</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                            <div className="row g-4">
+                                <div className="col-lg-3">
+                                    <div className="row g-4">
+                                        {/* <ProductFilter /> */}
+                                    </div>
+                                </div>
+
+
+                                {/* <ProductListingSearch /> */}
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
 }
 
+
+
 export const ProductDetails = () => {
-    return <h1>PLIST</h1>
+    return <h1>P DetailsLIST</h1>
 }
 
 
